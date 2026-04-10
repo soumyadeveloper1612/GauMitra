9da73ccccdd88294dcca04e6dbe24ee53a6eae98
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2026-04-10 16:04:38 +0530

    a

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
+Route::get('/test', function () {
+    return response()->json([
+        'status' => true,
+        'message' => 'API working',
+    ]);
+});
+
 Route::prefix('auth')->group(function () {
     Route::post('/send-otp', [AuthController::class, 'sendOtp']);
     Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
