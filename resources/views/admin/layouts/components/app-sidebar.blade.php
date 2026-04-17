793da9cf2f60223c5fcb3a3ebd8c2b73bb9379fb
soumyadeveloper1612 <soumyadeveloper1612@gmail.com>
2026-04-17 23:40:22 +0530

    h

--- resources/views/admin/layouts/components/app-sidebar.blade.php
+++ resources/views/admin/layouts/components/app-sidebar.blade.php
@@ -2,6 +2,7 @@
     $reportMenuOpen = request()->routeIs('admin.report-cases.*');
     $adminMenuOpen = request()->routeIs('admin.admins.*') || request()->routeIs('admin.roles.*');
     $gaushalaMenuOpen = request()->routeIs('admin.gaushalas.*');
+    $newsNoticeMenuOpen = request()->routeIs('admin.news-notices.*');
 @endphp
 
 <aside class="sidebar" id="sidebar">
@@ -97,6 +98,33 @@
                 </ul>
             </li>
 
+            <li class="menu-item has-submenu {{ $newsNoticeMenuOpen ? 'open' : '' }}">
+                <a href="javascript:void(0)" class="menu-link submenu-toggle {{ $newsNoticeMenuOpen ? 'active' : '' }}">
+                    <span class="nav-icon"><i class="bi bi-megaphone-fill"></i></span>
+                    <span class="nav-text">News & Notices</span>
+                    <span class="nav-badge">New</span>
+                    <span class="submenu-arrow"><i class="bi bi-chevron-down"></i></span>
+                </a>
+
+                <ul class="submenu" style="{{ $newsNoticeMenuOpen ? 'display:block;' : 'display:none;' }}">
+                    <li>
+                        <a href="{{ route('admin.news-notices.create') }}"
+                            class="{{ request()->routeIs('admin.news-notices.create') ? 'active' : '' }}">
+                            <span class="submenu-dot"></span>
+                            <span>Create News & Notice</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="{{ route('admin.news-notices.index') }}"
+                            class="{{ request()->routeIs('admin.news-notices.index') ? 'active' : '' }}">
+                            <span class="submenu-dot"></span>
+                            <span>Manage News & Notices</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
             <li class="menu-item has-submenu {{ $adminMenuOpen ? 'open' : '' }}">
                 <a href="javascript:void(0)" class="menu-link submenu-toggle {{ $adminMenuOpen ? 'active' : '' }}">
                     <span class="nav-icon"><i class="bi bi-shield-lock-fill"></i></span>
@@ -118,26 +146,6 @@
                         </a>
                     </li>
                 </ul>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#newsNoticeMenu" role="button"
-                    aria-expanded="false">
-                    <i class="bi bi-megaphone"></i>
-                    <span>News & Notices</span>
-                </a>
-                <div class="collapse" id="newsNoticeMenu">
-                    <ul class="nav flex-column ms-3">
-                        <li class="nav-item">
-                            <a href="{{ route('admin.news-notices.create') }}" class="nav-link">Create News &
-                                Notice</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('admin.news-notices.index') }}" class="nav-link">Manage News &
-                                Notices</a>
-                        </li>
-                    </ul>
-                </div>
             </li>
 
             <li class="menu-item">
